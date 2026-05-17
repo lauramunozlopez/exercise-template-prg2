93a351ad1dc3e6c52c13c9282721f79cc9f04371
ggg <you@example.com>
2026-05-17 16:29:33 +0200

    ooa

--- src/main/java/at/ac/fhcampuswien/services/MovieService.java
+++ src/main/java/at/ac/fhcampuswien/services/MovieService.java
@@ -14,7 +14,7 @@ public class MovieService {
 
     private MovieRepository movieRepository;
 
-    // Repository wird per Konstruktor injiziert (gut für Tests + Mockito) yoa
+    // Repository wird per Konstruktor injiziert (gut für Tests + Mockito) yoaa
     public MovieService(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
